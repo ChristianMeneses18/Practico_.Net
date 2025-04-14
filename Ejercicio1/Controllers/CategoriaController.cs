ef4fe69b5cbbf25a8ba6d7b2c4ad701ad6ab0636
chrismanu <christianmeneses617@gmail.com>
2025-04-14 15:40:55 -0300

    add modificar y eliminar productos categorias

--- Ejercicio1/Controllers/CategoriaController.cs
+++ Ejercicio1/Controllers/CategoriaController.cs
@@ -1,6 +1,7 @@
 ﻿using Ejercicio1.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Ejercicio1.Controllers
 {
@@ -13,17 +14,72 @@ namespace Ejercicio1.Controllers
 
         public IActionResult Create()
         {
+            ViewBag.Categorias = CategoriaProducto.Categorias;
             return View();
         }
 
         [HttpPost]
         public IActionResult Create(Categoria categoria)
         {
-            if (!ModelState.IsValid)
-                return View(categoria);
+            try
+            {
+                // Asigna la fecha actual si no se proporciona
+                if (categoria.FechaCreacion == default)
+                {
+                    categoria.FechaCreacion = DateOnly.FromDateTime(DateTime.Now); // Si no se especifica, se asigna la fecha de hoy
+                }
 
-            CategoriaProducto.AgregarCategoria(categoria);
-            return RedirectToAction("Index");
+                // Agregar la nueva categoría a la lista estática
+                CategoriaProducto.AgregarCategoria(categoria);
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception e)
+            {
+                ModelState.AddModelError("", e.Message);
+                ViewBag.Categorias = CategoriaProducto.Categorias;
+                return View(categoria);
+            }
         }
+
+        public IActionResult Edit(int id)
+        {
+            var categoria = CategoriaProducto.Categorias.FirstOrDefault(p => p.Id == id);
+            if (categoria == null)
+                return NotFound();
+
+            return PartialView("_EditarCategoriaPartial", categoria);
+        }
+
+        [HttpPost]
+        public IActionResult EditarPost(Categoria p)
+        {
+            var existente = CategoriaProducto.Categorias.FirstOrDefault(x => x.Id == p.Id);
+            if (existente != null)
+            {
+                // Actualiza los valores del producto con los datos enviados
+                existente.Nombre = p.Nombre;
+                existente.FechaCreacion = p.FechaCreacion;
+            }
+
+            return RedirectToAction("Index"); // Redirige al índice para ver los cambios
+        }
+
+        [HttpPost]
+        public IActionResult Eliminar(int id)
+        {
+            var existente = CategoriaProducto.Categorias.FirstOrDefault(x => x.Id == id);
+            if (existente == null)
+                return NotFound();
+
+            if (CategoriaProducto.Productos.Any(p => p.CategoriaId == id))
+            {
+                return BadRequest("No se puede eliminar la categoría porque tiene productos asociados.");
+            }
+
+            CategoriaProducto.Categorias.Remove(existente);
+            return Ok();
+        }
+
     }
 }
--- Ejercicio1/Controllers/ProductoController.cs
+++ Ejercicio1/Controllers/ProductoController.cs
@@ -1,6 +1,7 @@
 ﻿using Ejercicio1.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Ejercicio1.Controllers
 {
@@ -31,6 +32,46 @@ namespace Ejercicio1.Controllers
                 ViewBag.Categorias = CategoriaProducto.Categorias;
                 return View(producto);
             }
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var producto = CategoriaProducto.Productos.FirstOrDefault(p => p.Id == id);
+            if (producto == null)
+                return NotFound();
+
+            ViewBag.Categorias = new SelectList(CategoriaProducto.Categorias, "Id", "Nombre");
+
+            return PartialView("_EditarProductoPartial", producto);
+        }
+
+        [HttpPost]
+        public IActionResult EditarPost(Producto p)
+        {
+            var existente = CategoriaProducto.Productos.FirstOrDefault(x => x.Id == p.Id);
+            if (existente != null)
+            {
+                // Actualiza los valores del producto con los datos enviados
+                existente.Nombre = p.Nombre;
+                existente.Precio = p.Precio;
+                existente.FechaAlta = p.FechaAlta;
+                existente.CategoriaId = p.CategoriaId;
+                existente.Categoria = CategoriaProducto.Categorias.FirstOrDefault(c => c.Id == p.CategoriaId);
+            }
+
+            return RedirectToAction("Index"); // Redirige al índice para ver los cambios
+        }
+
+
+        [HttpPost]
+        public IActionResult Eliminar(int id)
+        {
+            var existente = CategoriaProducto.Productos.FirstOrDefault(x => x.Id == id);
+            if (existente == null)
+                return NotFound();
+
+            CategoriaProducto.Productos.Remove(existente);
+            return Ok();
         }
     }
 }
